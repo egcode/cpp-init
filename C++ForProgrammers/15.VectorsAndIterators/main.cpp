cce476353fc3824a912c64cde4c9509e9632d38c
eg <eugene@arcules.com>
2019-04-12 14:51:33 -0700

    iterator

--- C++ForProgrammers/15.VectorsAndIterators/main.cpp
+++ C++ForProgrammers/15.VectorsAndIterators/main.cpp
@@ -13,13 +13,22 @@ int main () {
   std::cout<<"\n\nAdding four elements to the vector\n";
   //assigning the value 3 to 4 elements of the vector
   vectorInts.assign(4,3);
+  for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
+    std::cout<<*it<<" ";
+  
+
+  std::cout<<"\n\nAdding four elements to the vector\n";
+  //assigning the value 3 to 4 elements of the vector
+  vectorInts.push_back(24);
+  vectorInts.push_back(25);
+  vectorInts.push_back(26);
+  vectorInts.push_back(27);
   std::cout<<"vectorInts has "<<vectorInts.size()<<" elements\n";
   
   //printing the contents of vectorInts
   std::cout<<"VectorInts has these elements:\n";
   for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
     std::cout<<*it<<" ";
-
+ 
   return 0;
 }
-
