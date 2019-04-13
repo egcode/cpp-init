2eb013c598f987aeaeeb117e730ae63dc77bb77f
Eugene G <you@example.com>
2019-04-13 16:24:46 -0700

    vector

---
+++ 2.C++Programming/main3_2d_matrix.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    // declare 2 dimentional vector of type int
+    vector < vector<int> > twodvector;
+
+    // setup a row 
+    vector<int> singlerow(3,2);
+
+    // append five rows
+    for(int i; i<5; i++) {
+        twodvector.push_back(singlerow);
+    }
+
+    // print out the matrix
+    for(int row=0; row<twodvector.size(); row++) {
+        for(int column=0; column<twodvector[0].size(); column++) {
+            cout<<twodvector[row][column]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
